b648bff7f6372f14c27bf249094c55af7ce854d9
mystified131 <mystifiedthomas@gmail.com>
2018-11-01 18:24:03 -0500

    Initial Commit

--- Area2/Program.cs
+++ Area2/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 
-namespace Area2
+namespace Area
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double area;
+            double radius;
+
+            Console.WriteLine("Let's calculate the area of a circle!");
+            Console.WriteLine("The radius of the circle is:");
+            radius = double.Parse(Console.ReadLine());
+
+            if (radius > 0) { 
+
+            area = CalculateArea(radius);
+                Console.WriteLine("A circle with the radius " + radius + " is a total of " + area);
+                Console.ReadLine();
+            }
+
+            else
+
+            {
+                Console.WriteLine("The number you entered was negative, so no calculation could be made.");
+                Console.ReadLine();
+
+            }
+
+        }
+
+        private static double CalculateArea(double radius)
+            {
+               double area = 3.14 * (radius* radius);
+            return area;
+
+            }
+
         }
     }
-}
+
