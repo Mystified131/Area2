a084e1c27747bfa7b75a0fed778989acf4090d21
mystified131 <mystifiedthomas@gmail.com>
2018-11-01 18:12:09 -0500

    Add project files.

---
+++ Area2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Area2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
